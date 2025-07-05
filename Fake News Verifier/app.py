a0aa358fbcc255fe599c3e3a36fb01712ebbad55
Mahalakshmi Gounder <mahalakshmigounder72@gmail.com>
2025-07-06 01:57:41 +0530

    Update app.py

--- Fake News Verifier/app.py
+++ Fake News Verifier/app.py
@@ -191,5 +191,7 @@ def verify_news(input_text, language):
     return result
 
 
-if(__name__=='__main__'):
-    app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use the environment variable PORT or fallback to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
